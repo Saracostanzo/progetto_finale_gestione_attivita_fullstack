2df9bc996e65b3fee99f155964770b6f0701bce1
eleonoradan <eleonorayotobiana@gmail.com>
2026-04-08 17:07:25 +0200

    Errore.java, UserController.java(incompleto), TaskController.java

--- progettofinale/src/main/java/it/itconsulting/progettofinale/controller/TaskController.java
+++ progettofinale/src/main/java/it/itconsulting/progettofinale/controller/TaskController.java
@@ -1,10 +1,85 @@
 package it.itconsulting.progettofinale.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import it.itconsulting.progettofinale.dto.TaskDto;
+import it.itconsulting.progettofinale.model.Errore;
+import it.itconsulting.progettofinale.model.Task;
+import it.itconsulting.progettofinale.service.TaskService;
+
+
+
 
 @RestController
 @CrossOrigin(origins = {"http://127.0.0.1:5501", "http://localhost:5501"})
 public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping("/api/tasks")
+    public List<Task> getAll() {
+        return taskService.getAll();
+    }
+
+    @PostMapping("/api/tasks")
+    public ResponseEntity<Object> create(@RequestBody(required=false) @Validated TaskDto tDto, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            Errore errore = new Errore();
+            errore.setMessaggio(bindingResult.getAllErrors().stream().map(obError -> obError.getDefaultMessage()).collect(Collectors.joining(","))); 
+            errore.setDataErrore(LocalDateTime.now());
+            return ResponseEntity.badRequest().body(errore); 
+        }
+        Task task = null;
+        try {
+            task = taskService.create(tDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(task);
+        } catch (IllegalArgumentException e) {
+            Errore error = new Errore();
+            error.setMessaggio(e.getMessage());
+            error.setDataErrore(LocalDateTime.now());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error); 
+        }   
+    }
+
+    @PutMapping("/api/tasks/{id}")
+    public ResponseEntity<Object> update(@RequestBody(required=false) @Validated TaskDto tDto, BindingResult bindingResult, @PathVariable long id) {
+        if(bindingResult.hasErrors()) {
+            Errore errore = new Errore();
+            errore.setMessaggio(bindingResult.getAllErrors().stream().map(obError -> obError.getDefaultMessage()).collect(Collectors.joining(","))); 
+            errore.setDataErrore(LocalDateTime.now());
+            return ResponseEntity.badRequest().body(errore); 
+        }
+        Task task = null;
+        try {
+            task = taskService.update(tDto, id);
+            return ResponseEntity.status(HttpStatus.CREATED).body(task);
+        } catch (IllegalArgumentException e) {
+            Errore error = new Errore();
+            error.setMessaggio(e.getMessage());
+            error.setDataErrore(LocalDateTime.now());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error); 
+        } 
+    }
     
+    @DeleteMapping("/api/tasks/{id}") 
+    public void delete(@PathVariable long id){
+      taskService.delete(id);
+    }
+
 }
--- progettofinale/src/main/java/it/itconsulting/progettofinale/controller/UserController.java
+++ progettofinale/src/main/java/it/itconsulting/progettofinale/controller/UserController.java
@@ -1,10 +1,51 @@
 package it.itconsulting.progettofinale.controller;
 
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import it.itconsulting.progettofinale.dto.UserDto;
+import it.itconsulting.progettofinale.model.Errore;
+import it.itconsulting.progettofinale.model.User;
+import it.itconsulting.progettofinale.service.UserService;
 
 @RestController
 @CrossOrigin(origins = {"http://127.0.0.1:5501", "http://localhost:5501"})
 public class UserController {
-    
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/api/users/register")
+    public ResponseEntity<Object> register(@RequestBody(required=false) @Validated UserDto uDto, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            Errore errore = new Errore();
+            errore.setMessaggio(bindingResult.getAllErrors().stream().map(obError -> obError.getDefaultMessage()).collect(Collectors.joining(","))); 
+            errore.setDataErrore(LocalDateTime.now());
+            return ResponseEntity.badRequest().body(errore); 
+        }
+        User user = null;
+        try {
+            user = userService.create(uDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(user);
+        } catch (IllegalArgumentException e) {
+            Errore error = new Errore();
+            error.setMessaggio(e.getMessage());
+            error.setDataErrore(LocalDateTime.now());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error); 
+        }  
+    }
+
+    @PostMapping("/api/users/login")
+    public ResponseEntity<Object> login(@RequestBody(required=false) @Validated UserDto uDto, BindingResult bindingResult) {
+        
+    }
 }
